d88d92c8284905c8ee3a8915d073b2ecaf598f51
Cyrus Zei <cyruszei@gmail.com>
2016-08-26 07:59:41 +0200

    first

---
+++ api/controllers/PorkController.js
@@ -0,0 +1,11 @@
+/**
+ * PorkController
+ *
+ * @description :: Server-side logic for managing porks
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
